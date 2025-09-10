8a2f37a88caa429a8d5f0eb864240ffcaf15fe53
skydiver89 <maslovil@rambler.ru>
2025-09-10 19:32:55 +0300

    deleted buildrun.sh

--- buildrun.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export VOSK_PATH=`pwd`/vosk-linux-x86_64-0.3.45
-export LD_LIBRARY_PATH=$VOSK_PATH
-export CGO_CPPFLAGS="-I$VOSK_PATH"
-export CGO_LDFLAGS="-L $VOSK_PATH"
-go build
-./sp2tbot
-exit 0
